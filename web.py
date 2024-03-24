7200aa22afc95c4c8bafdaf172462a41d0741f96
James Murray <the.james.murray@gmail.com>
2024-03-24 04:28:21 +0000

    Add basic search functionality
    Just a wildcard match for now.

--- web.py
+++ web.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
 
-from family_tree import Family
+from family_tree import Family, Person
 
 app = Flask(__name__)
+
+@app.route('/search/')
+def search():
+    query = escape(request.args.get('query'))
+    return render_template('search.html', results=Person.search(query))
 
 @app.route('/<int:id>')
 def person_page(id):
