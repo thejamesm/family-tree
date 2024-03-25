983745cf5829db744debac08306652cd232a0af3
James Murray <the.james.murray@gmail.com>
2024-03-25 00:52:57 +0000

    Add portrait pictures where available
    Check for a JPEG matching the person ID in /static/images

--- web.py
+++ web.py
@@ -1,3 +1,5 @@
+import os.path
+
 from flask import Flask, render_template, request
 from markupsafe import escape
 
@@ -13,5 +15,10 @@ def search():
 @app.route('/<int:id>')
 def person_page(id):
     family = Family()
-    person = family.add_person(id)
-    return render_template('person.html', person=person)
+    person = family.person(id)
+    if os.path.isfile(os.path.join('static', 'images', f'{id}.jpg')):
+        img_filename = f'images/{id}.jpg'
+    else:
+        img_filename = None
+    return render_template('person.html', person=person,
+                           img_filename=img_filename)
