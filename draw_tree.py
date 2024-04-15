802d4a815d6f1b7c46761aefb2c09d1df402fabe
James Murray <the.james.murray@gmail.com>
2024-04-15 17:04:12 +0100

    Refactor digram drawing script
    - Extend Digraph class
    - Override `.node()` and `.edge()` methods with some
      application-specific argument processing
      - `.node()` accepts Person in place of ID and extracts appropriate
        fields accordingly
      - `invis` and `weight` parameters, rather than accessing direclty
        through attributes object

--- draw_tree.py
+++ draw_tree.py
@@ -1,33 +1,41 @@
 from graphviz import Digraph
 
-from family_tree import Family
+from family_tree import Family, Person
 
 def url_for(page, id):
     return str(id)
 
-class Tree:
-    def __init__(self, subject):
-        self.invis_node = {'label': '',
-                           '_attributes': {'shape': 'point',
-                                           'peripheries': '0',
-                                           'height': '0',
-                                           'width': '0'}}
-        self.invis_edge = {'_attributes': {'style': 'invis'}}
-        self.married_edge = {}
-        self.unmarried_edge = {'_attributes': {'style': 'dashed'}}
+class TreeGraph(Digraph):
+    MARRIED_EDGE = {}
+    UNMARRIED_EDGE = {'style': 'dashed'}
 
-        self.graph = Digraph('neato',
-                             format='svg',
-                             filename=f'static/trees/{subject.id}',
-                             graph_attr={'splines': 'ortho'},
-                             node_attr={'shape': 'box',
-                                        'style': 'filled'},
-                             edge_attr={'dir': 'none'})
+    def node(self, id, label='', attributes={}, invis=False, **kwargs):
+        if type(id) is Person:
+            person = id
+            id = person.id
+            label = label or person.name
+            attributes = attributes | {
+                    'fillcolor': self.node_fill(person.gender),
+                    'URL': url_for('person_page', id=person.id)
+                }
+        if invis:
+            attributes = attributes | {'shape': 'point',
+                                       'peripheries': '0',
+                                       'height': '0',
+                                       'width': '0'}
+        id = str(id)
+        attributes = {k: str(v) for k, v in attributes.items()}
+        Digraph.node(self, id, label=label, _attributes=attributes, **kwargs)
 
-        layers = subject.get_layers()
-        self.draw_layer(layers[0], first_layer=True)
-        for layer in layers[1:]:
-            self.draw_layer(layer)
+    def edge(self, node_a, node_b, attributes={}, weight=1, invis=False,
+             **kwargs):
+        if invis:
+            attributes = attributes | {'style': 'invis'}
+        node_a = str(node_a)
+        node_b = str(node_b)
+        attributes = attributes | {'weight': weight}
+        attributes = {k: str(v) for k, v in attributes.items()}
+        Digraph.edge(self, node_a, node_b, _attributes=attributes, **kwargs)
 
     def node_fill(self, gender):
         match gender:
@@ -38,10 +46,20 @@ class Tree:
             case _:
                 return 'gray'
 
-    def draw_person_node(self, graph, person):
-        graph.node(str(person.id), label=person.name,
-                _attributes={'fillcolor': self.node_fill(person.gender),
-                             'URL': url_for('person_page', id=person.id)})
+class Tree:
+    def __init__(self, subject):
+        self.graph = TreeGraph('dot',
+                               format='svg',
+                               filename=f'static/trees/{subject.id}',
+                               graph_attr={'splines': 'ortho'},
+                               node_attr={'shape': 'box',
+                                          'style': 'filled'},
+                               edge_attr={'dir': 'none'})
+
+        layers = subject.get_layers()
+        self.draw_layer(layers[0], first_layer=True)
+        for layer in layers[1:]:
+            self.draw_layer(layer)
 
     def draw_layer(self, layer, first_layer=False):
         if not first_layer:
@@ -50,43 +68,42 @@ class Tree:
                                           if g[0]]:
                     people = list(group)
                     if len(people) == 1:
-                        line_subgraph.edge(str(parents_id), str(people[0].id))
+                        line_subgraph.edge(parents_id, people[0].id)
                     else:
                         head_nodes = []
                         for person in people[1:-1]:
                             node_id = f'n{person.id}'
-                            line_subgraph.node(node_id, **self.invis_node)
-                            line_subgraph.edge(node_id, str(person.id))
+                            line_subgraph.node(node_id, invis=True)
+                            line_subgraph.edge(node_id, person.id)
                             head_nodes.append(node_id)
                         n_nodes = len(head_nodes)
                         if n_nodes % 2 == 0:
                             node_id = f'b{parents_id}'
                             head_nodes.insert(n_nodes // 2, node_id)
-                            line_subgraph.node(node_id, **self.invis_node)
+                            line_subgraph.node(node_id, invis=True)
                         line_subgraph.edge(parents_id, head_nodes[n_nodes // 2])
                         for prev_id, cur in zip(head_nodes, head_nodes[1:]):
                             line_subgraph.edge(prev_id, cur)
-                        line_subgraph.edge(head_nodes[0], str(people[0].id))
-                        line_subgraph.edge(head_nodes[-1], str(people[-1].id))
+                        line_subgraph.edge(head_nodes[0], people[0].id)
+                        line_subgraph.edge(head_nodes[-1], people[-1].id)
         with self.graph.subgraph() as person_subgraph:
             person_subgraph.attr(rank='same')
             for people in layer['groups'].values():
                 for person in people:
-                    self.draw_person_node(person_subgraph, person)
+                    person_subgraph.node(person)
             prev_id = None
             for couple_id, (left, right) in layer['edges'].items():
                 if ((relationship := family.get_relationship(left, right)) and
                         relationship.type == 'marriage'):
-                    join_style = self.married_edge
+                    join_style = TreeGraph.MARRIED_EDGE
                 else:
-                    join_style = self.unmarried_edge
-                person_subgraph.node(couple_id, **self.invis_node)
-                person_subgraph.edge(str(left.id), couple_id, **join_style)
-                person_subgraph.edge(couple_id, str(right.id), **join_style)
+                    join_style = TreeGraph.UNMARRIED_EDGE
+                person_subgraph.node(couple_id, invis=True)
+                person_subgraph.edge(left.id, couple_id, join_style)
+                person_subgraph.edge(couple_id, right.id, join_style)
                 if prev_id:
-                    person_subgraph.edge(prev_id, str(left.id),
-                                         **self.invis_edge)
-                prev_id = str(right.id)
+                    person_subgraph.edge(prev_id, left.id, invis=True)
+                prev_id = right.id
 
 
 if __name__ == '__main__':
