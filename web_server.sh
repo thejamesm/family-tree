f07b51d551142aaeaa47d7197790603626a71e31
James <thejamesm@users.noreply.github.com>
2025-01-22 16:20:27 +0000

    Include Docker launch in startup script

--- web_server.sh
+++ web_server.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-/var/www/family_tree/.venv/bin/gunicorn -w 8 -b 0.0.0.0 'web:app'
+#/var/www/family_tree/.venv/bin/gunicorn -w 8 -b 0.0.0.0 'web:app'
+sudo service docker start
+docker start family-tree
+/var/www/family_tree/.venv/bin/gunicorn --chdir /var/www/family_tree -w 8 -b 0.0.0.0 'web:app'
